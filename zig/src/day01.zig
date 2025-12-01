288a9e918f5d0235e660933574f67ec5498b50aa
Bujor Mihai Alexandru <bujormihaialexandru@gmail.com>
2025-12-01 12:00:08 +0200

    zig day 01

--- zig/src/day01.zig
+++ zig/src/day01.zig
@@ -1,13 +1,57 @@
 const std = @import("std");
+const utils = @import("utils.zig");
 
 pub fn part1(allocator: std.mem.Allocator, input: []const u8) ![]const u8 {
-    _ = input;
-    // TODO: Implement solution for day 01 part 1
-    return try allocator.dupe(u8, "Not implemented");
+    var password: i32 = 0;
+    var dialPosition: i32 = 50;
+    var rotations = try utils.lines(allocator, input);
+    defer rotations.deinit(allocator);
+
+    for (rotations.items) |rotation| {
+        const direction = rotation[0];
+        const distance = @mod(try std.fmt.parseInt(i32, rotation[1..], 10), 100);
+
+        if (direction == 'L') {
+            dialPosition -= distance;
+        } else {
+            dialPosition += distance;
+        }
+
+        dialPosition = @mod(dialPosition, 100);
+
+        if (dialPosition == 0) {
+            password += 1;
+        }
+    }
+
+    return try std.fmt.allocPrint(allocator, "{d}", .{password});
 }
 
 pub fn part2(allocator: std.mem.Allocator, input: []const u8) ![]const u8 {
-    _ = input;
-    // TODO: Implement solution for day 01 part 2
-    return try allocator.dupe(u8, "Not implemented");
+    var password: i32 = 0;
+    var dialPosition: i32 = 50;
+    var rotations = try utils.lines(allocator, input);
+    defer rotations.deinit(allocator);
+
+    for (rotations.items) |rotation| {
+        const direction = rotation[0];
+        var distance = try std.fmt.parseInt(i32, rotation[1..], 10);
+
+        password += @divFloor(distance, 100);
+        distance = @mod(distance, 100);
+
+        if (direction == 'L') {
+            if (dialPosition - distance <= 0 and dialPosition != 0) {
+                password += 1;
+            }
+            dialPosition = @mod(dialPosition - distance, 100);
+        } else {
+            if (dialPosition + distance >= 100 and dialPosition != 0) {
+                password += 1;
+            }
+            dialPosition = @mod(dialPosition + distance, 100);
+        }
+    }
+
+    return try std.fmt.allocPrint(allocator, "{d}", .{password});
 }
--- zig/src/utils.zig
+++ zig/src/utils.zig
@@ -1,3 +1,10 @@
 const std = @import("std");
 
-// Add your utility functions here
+pub fn lines(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList([]const u8) {
+    var result = try std.ArrayList([]const u8).initCapacity(allocator, 0);
+    var iter = std.mem.splitScalar(u8, input, '\n');
+    while (iter.next()) |line| {
+        try result.append(allocator, line);
+    }
+    return result;
+}
