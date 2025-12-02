a9694e0b0bc9dc4562f9095f3692277e7beabc6b
Bujor Mihai Alexandru <bujormihaialexandru@gmail.com>
2025-12-02 13:28:44 +0200

    kotlin day 02

--- kotlin/src/main/kotlin/day02/Solution.kt
+++ kotlin/src/main/kotlin/day02/Solution.kt
@@ -1,11 +1,127 @@
 package day02
 
+import java.math.BigInteger
+import kotlin.collections.emptyList
+import kotlin.text.toBigInteger
+import utils.getProperDivisors
+import utils.concatTimes
+
 fun part1(input: String): String {
-    // TODO: Implement solution for day 02 part 1
-    return "Not implemented"
+    val ranges = input.split(",")
+    var sum = BigInteger("0")
+    val one = BigInteger("1")
+
+    ranges.forEach {
+        val (start, end) = it.split("-")
+        var len = start.length
+        var firstHalf = start.subSequence(0, len/2)
+        if (len == 1) {
+            firstHalf = start.subSequence(0, len)
+        }
+        var currentNumber = "$firstHalf$firstHalf".toBigInteger()
+        val rangeStart = start.toBigInteger()
+        val rangeEnd = end.toBigInteger()
+        while (currentNumber <= rangeEnd) {
+            if (currentNumber >= rangeStart) {
+                sum += currentNumber
+            }
+            len = currentNumber.toString().length
+            firstHalf = currentNumber.toString().subSequence(0, len/2).toString()
+            var halfNumber = firstHalf.toBigInteger()
+            halfNumber += one
+            firstHalf = halfNumber.toString()
+            currentNumber = "$firstHalf$firstHalf".toBigInteger()
+        }
+    }
+
+    return sum.toString()
 }
 
 fun part2(input: String): String {
-    // TODO: Implement solution for day 02 part 2
-    return "Not implemented"
+    val ranges = input.split(",")
+    val rg = Regex("([0-9]+)\\1+")
+    var sum = BigInteger("0")
+
+    ranges.forEach {
+        val (start, end) = it.split("-").map { it.toBigInteger() }
+
+        var num = start
+        while (num <= end) {
+            if (rg.matches(num.toString())) {
+                sum += num
+            }
+            num++
+        }
+    }
+    return sum.toString()
+
+    // the complicated solution that didnt work
+
+    // val ranges = input.split(",")
+    // val one = BigInteger("1")
+    // var result = emptySet<BigInteger>().toMutableSet()
+
+    // ranges.forEach {
+    //     val (start, end) = it.split("-")
+
+    //     val seqSizes = emptySet<Int>().toMutableSet()
+    //     for (len in start.length..end.length) {
+    //         val divs = getProperDivisors(len)
+    //         for (div in divs) {
+    //             seqSizes.add(div)
+    //         }
+    //     }
+
+    //     println("$start-$end:\n  ${seqSizes.toList()}")
+
+
+    //     for (size in seqSizes) {
+    //         for (len in start.length..end.length) {
+    //             if (len % size != 0) {
+    //                 continue
+    //             }
+    
+    //             var correctSeq = if (len == start.length) {
+    //                 start
+    //             } else if (len == end.length) {
+    //                 end
+    //             } else {
+    //                 concatTimes("1".toString(), len)
+    //             }
+
+    //             val amount = len / size
+    //             var seq = correctSeq.subSequence(0, size).toString()
+    //             var currentNumber = concatTimes(seq, amount).toBigInteger()
+    //             val rangeStart = start.toBigInteger()
+    //             val rangeEnd = end.toBigInteger()
+    //             var passes = 0
+
+    //             println("  size $size\n  amount $amount")
+
+    //             while (currentNumber <= rangeEnd) {
+    //                 if (len == 1 && passes > 10) {
+    //                     break
+    //                 }
+    //                 if (currentNumber >= rangeStart) {             
+    //                     println("\t$currentNumber")           
+    //                     result.add(currentNumber)
+    //                 }
+    //                 val seqNum = currentNumber.toString().subSequence(0, size).toString().toBigInteger() + one
+    //                 seq = seqNum.toString()
+    //                 currentNumber = concatTimes(seq, amount).toBigInteger()
+    //                 passes++
+    //             }
+    //         }
+    //     }
+    // }
+
+    // return result.toList().sum().toString();
 }
+
+// fun Iterable<BigInteger>.sum(): BigInteger {
+//     var result = BigInteger("0")
+//     for (num in this) {
+//         result += num
+//     }
+//     return result
+// }
--- kotlin/src/main/kotlin/utils/Utils.kt
+++ kotlin/src/main/kotlin/utils/Utils.kt
@@ -1,3 +1,19 @@
 package utils
 
-// Add your utility functions here
+fun getProperDivisors(num: Int): List<Int> {
+    val result = emptyList<Int>().toMutableList()
+    for (div in num/2 downTo 1) {
+        if (num % div == 0) {
+            result.add(div)
+        }
+    }
+    return result
+}
+
+fun concatTimes(str: String, times: Int): String {
+    var result = ""
+    for (i in 1..times) {
+        result += str
+    }
+    return result
+}
