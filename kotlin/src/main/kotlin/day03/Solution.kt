a243d3bd869bbfa1e1837f4b92be84976e350983
Bujor Mihai Alexandru <bujormihaialexandru@gmail.com>
2025-12-03 09:38:38 +0200

    kotlin day 03

--- kotlin/src/main/kotlin/day03/Solution.kt
+++ kotlin/src/main/kotlin/day03/Solution.kt
@@ -1,11 +1,59 @@
 package day03
 
+import java.math.BigInteger
+
 fun part1(input: String): String {
-    // TODO: Implement solution for day 03 part 1
-    return "Not implemented"
+    val banks = input.lines()
+    var sum = 0
+
+    banks.forEach {
+        var left = 0
+        var right = 0
+        for ((pos, rating) in it.withIndex()) {
+            val ratingInt = rating.digitToInt()
+            if (ratingInt > left && pos != it.length-1) {
+                left = ratingInt
+                right = 0
+            }
+            else if (ratingInt > right) {
+                right = ratingInt
+            }
+        }
+        sum += "$left$right".toInt()
+    }
+
+    return sum.toString()
 }
 
 fun part2(input: String): String {
-    // TODO: Implement solution for day 03 part 2
-    return "Not implemented"
+    val banks = input.lines()
+    var sum = BigInteger("0")
+
+    banks.forEach {
+        var number = ""
+        var searchSpaceEnd = it.length-12
+        var searchSpaceStart = 0
+        while (number.length < 12) {
+            val (digit, index) = it.getMax(searchSpaceStart, searchSpaceEnd)
+            number += digit.toString()
+            searchSpaceStart = index+1
+            searchSpaceEnd++
+        }
+        sum += number.toBigInteger()    
+    }
+
+    return sum.toString()
+}
+
+fun String.getMax(startIndex: Int, endIndex: Int): Pair<Int, Int> {
+    var max = this[startIndex].digitToInt()
+    var maxIndex = startIndex
+    for (i in startIndex+1..endIndex) {
+        val elem = this[i].digitToInt()
+        if (elem > max) {
+            max = elem
+            maxIndex = i
+        }
+    }
+    return Pair(max, maxIndex)
 }
