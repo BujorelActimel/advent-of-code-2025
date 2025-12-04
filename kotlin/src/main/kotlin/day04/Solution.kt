64ad39e0687d82d82fa9c60a46826f6f91118a67
Bujor Mihai Alexandru <bujormihaialexandru@gmail.com>
2025-12-04 08:33:08 +0200

    kotlin day 04

--- kotlin/src/main/kotlin/day04/Solution.kt
+++ kotlin/src/main/kotlin/day04/Solution.kt
@@ -1,11 +1,57 @@
 package day04
 
 fun part1(input: String): String {
-    // TODO: Implement solution for day 04 part 1
-    return "Not implemented"
+    var answer = 0
+    val lines = input.lines()
+    for ((i, line) in lines.withIndex()) {
+        for ((j, spot) in line.withIndex()) {
+            if (spot == '@' && countAdjRolls(lines, i, j) < 4) {
+                answer++
+            }
+        }
+    }
+    return answer.toString()
 }
 
 fun part2(input: String): String {
-    // TODO: Implement solution for day 04 part 2
-    return "Not implemented"
+    var answer = 0
+    var lines = input.lines().toMutableList()
+    do {
+        var changed = false
+        for ((i, line) in lines.withIndex()) {
+            var newLine = ""
+            for ((j, spot) in line.withIndex()) {
+                if (spot == '@' && countAdjRolls(lines, i, j) < 4) {
+                    answer++
+                    newLine += '.'
+                    changed=true
+                } else {
+                    newLine += spot
+                }
+            }
+            lines[i] = newLine
+        }
+    } while(changed)
+    return answer.toString()
+}
+
+fun countAdjRolls(lines: List<String>, line: Int, col: Int): Int {
+    val directions = listOf(0, -1, 1)
+    var count = 0
+    for (x in directions) {
+        for (y in directions) {
+            if (x != 0 || y != 0) {
+                if (
+                    line+x >= 0 && 
+                    line+x < lines.size &&
+                    col+y >= 0 &&
+                    col+y < lines[0].length &&
+                    lines[line+x][col+y] == '@'
+                ) {
+                    count++
+                }
+            }
+        }
+    }
+    return count
 }
