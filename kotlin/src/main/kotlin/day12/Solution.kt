7ec9a2eae5bb644ef78b9cea36d59b8b2d629511
Bujor Mihai Alexandru <bujormihaialexandru@gmail.com>
2025-12-12 13:03:46 +0200

    kotlin day 12 - lol?

--- kotlin/src/main/kotlin/day12/Solution.kt
+++ kotlin/src/main/kotlin/day12/Solution.kt
@@ -1,11 +1,27 @@
 package day12
 
 fun part1(input: String): String {
-    // TODO: Implement solution for day 12 part 1
-    return "Not implemented"
+    var ans = 0
+
+    for (line in input.lines()) {
+        if (": " in line) {
+            val (z, n) = line.split(": ")
+            val (x, y) = z.split("x").map { it.toInt() }1
+
+            val tiles = listOf(7, 7, 7, 5, 7, 6)
+            val nums = n.split(" ").map { it.toInt() }
+
+            val sumProducts = nums.zip(tiles).sumOf { (a, b) -> a * b }
+
+            if (x * y > sumProducts) {
+                ans += 1
+            }
+        }
+    }
+
+    return ans.toString()
 }
 
 fun part2(input: String): String {
-    // TODO: Implement solution for day 12 part 2
-    return "Not implemented"
+    return "hooray!?"
 }
